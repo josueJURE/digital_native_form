d066e4ddc61c17eb972d2f13fafeaa0cdff9ecc4
Josue Jure <jjure@CCS-MacBook14.local>
2025-02-26 17:28:36 +0000

    (feat: HTML) add input event and modify regexes

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
+const creditCardField = document.querySelector("#credit-card");
 
+function isValidLuhnNumber(numberString) {
+  let sum = 0;
+  let isSecond = false;
 
-function checkLuhnCardNumber (cardNumberString) {
-    let sum = 0;
-    let isSecond = false;
-
-    for (let i = cardNumberString.length - 1; i >= 0; i--) {
-      let digit = Number(cardNumberString[i]);
-      if (isSecond) {
-        digit = digit * 2;
-      }
-      // Add two digits to handle cases that make two cardNumberLength
-      // after doubling
-      sum += Math.floor(digit / 10);
-      sum += digit % 10;
-      isSecond = !isSecond;
+  for (let i = numberString.length - 1; i >= 0; i--) {
+    let digit = Number(numberString[i]);
+    if (isSecond) {
+      digit = digit * 2;
     }
-    return !!(sum % 10 === 0);
+    // Add two digits to handle cases that make two cardNumberLength
+    // after doubling
+    sum += Math.floor(digit / 10);
+    sum += digit % 10;
+    isSecond = !isSecond;
   }
+  return !!(sum % 10 === 0);
+}
+
+creditCardField.addEventListener("input", (event) => {
+  if (!isValidLuhnNumber(event.target.value)) {
+    creditCardField.setCustomValidity("That's not a valid credit card number");
+  } else {
+    creditCardField.setCustomValidity("");
+  }
+});
